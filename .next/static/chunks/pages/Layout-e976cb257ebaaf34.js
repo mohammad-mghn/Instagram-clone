99b1a2bd948282ad5e4e2c4ccc72a8da8db18c80
vito.mohagheghian <77550037+vito-mohagheghian@users.noreply.github.com>
2022-03-23 16:41:25 +0430

    Delete .next directory

--- .next/static/chunks/pages/Layout-e976cb257ebaaf34.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[511],{2853:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/Layout",function(){return u(432)}])}},function(n){n.O(0,[774,888,179],(function(){return _=2853,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
